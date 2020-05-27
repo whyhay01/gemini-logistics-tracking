c092fd4ceee0fae06afab1e623713851674e7574
Bello Yakub <hyaaquub@gmail.com>
2020-05-27 08:29:06 +0100

    chore:design-user-login-page

--- app/src/main/java/com/example/startnglogistics/LandingPage.java
+++ app/src/main/java/com/example/startnglogistics/LandingPage.java
@@ -2,13 +2,34 @@ package com.example.startnglogistics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingPage extends AppCompatActivity {
+
+    Button mButtonSignupShipper;
+    Button mButtonSignup;
+    Button mButtonlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        mButtonSignupShipper = findViewById(R.id.btn_sign_up_shipper);
+        mButtonSignup = findViewById(R.id.btn_signup);
+        mButtonlogin = findViewById(R.id.btn_login);
+
+        mButtonlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoginPage = new Intent(LandingPage.this, LoginPage.class);
+                startActivity(intentLoginPage);
+                finish();
+            }
+        });
+
     }
 }
